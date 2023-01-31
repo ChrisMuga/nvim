686c17ecb1db19cfe12f7203f582a92008a4be33
Chris Muga <chrismuga94@gmail.com>
2023-01-31 03:21:54 +0300

    better theme randomization

--- lua/set-theme.lua
+++ lua/set-theme.lua
@@ -1,14 +1,27 @@
 -- Select a random theme (according to time), and set it as the default
+-- New Theme Every Minute (when you fire up nvim, obviously, not realtime.)
 time = os.date("*t")
-hour = time.hour
+min = time.min
+
+if min == 0 then
+	min = 1
+end
 
 themes = {
 		
 		'terafox', 'nightfox', 'dawnfox', 'carbonfox', 'duskfox', 'nordfox', 
 		'terafox', 'nightfox', 'dawnfox', 'carbonfox', 'duskfox', 'nordfox', 
 		'terafox', 'nightfox', 'dawnfox', 'carbonfox', 'duskfox', 'nordfox', 
+		'terafox', 'nightfox', 'dawnfox', 'carbonfox', 'duskfox', 'nordfox',
+		'terafox', 'nightfox', 'dawnfox', 'carbonfox', 'duskfox', 'nordfox', 
+		'terafox', 'nightfox', 'dawnfox', 'carbonfox', 'duskfox', 'nordfox', 
+		'terafox', 'nightfox', 'dawnfox', 'carbonfox', 'duskfox', 'nordfox', 
+		'terafox', 'nightfox', 'dawnfox', 'carbonfox', 'duskfox', 'nordfox',
+		'terafox', 'nightfox', 'dawnfox', 'carbonfox', 'duskfox', 'nordfox', 
 		'terafox', 'nightfox', 'dawnfox', 'carbonfox', 'duskfox', 'nordfox' 
+
+
 	}
 
-random_theme = themes[hour]
+random_theme = themes[min]
 vim.cmd('colorscheme ' .. random_theme)
