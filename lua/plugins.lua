ea513c52b16f059cc155d46a19ea102e8da6e6e6
Chris Muga <chrismuga94@gmail.com>
2023-07-12 19:50:55 +0300

    search + replace

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -53,6 +53,7 @@ return require('packer').startup(function(use)
 	-- Fuzzy Finder (2)
 	use 'nvim-lua/plenary.nvim'
 	use 'nvim-telescope/telescope.nvim'
+	use 'nvim-pack/nvim-spectre'
 
 	-- Set Transparent Background
 	use 'xiyaowong/transparent.nvim'
