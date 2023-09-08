393faa89d324f198b484bb7ce7561c237fdd7ae3
Chris Muga <ChrisMuga94@Gmail.com>
2023-09-08 21:14:35 +0300

    surround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -64,6 +64,16 @@ return require('packer').startup(function(use)
 
 	-- Catppuccin Theme
 	use { "catppuccin/nvim", as = "catppuccin" }
+
+	use({
+	    "kylechui/nvim-surround",
+	    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+	    config = function()
+		require("nvim-surround").setup({
+		    -- Configuration here, or leave empty to use defaults
+        })
+    end
+})
 end)
 
 
