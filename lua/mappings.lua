e3695a2397fedc9659382f2dd572e995ba56dd73
Chris Muga <chrismuga94@gmail.com>
2023-09-13 20:03:44 +0300

    some telescope shortcuts

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,9 +11,11 @@ vim.keymap.set('n', '<C-f>', 'NERDTreeFind<CR>')
 vim.keymap.set('n', '<C-b>', 'BlamerToggle<CR>')
 
 -- Fuzzy Finder Mappings
-vim.keymap.set('n', 'ff', '<cmd>Telescope find_files<cr>')
-vim.keymap.set('n', 'fs', '<cmd>Telescope live_grep<cr>')
-vim.keymap.set('n', 'fb', '<cmd>Telescope buffers<cr>')
+vim.keymap.set('n', 'ff', '<cmd>Telescope find_files<cr>') -- Find Files
+vim.keymap.set('n', 'fs', '<cmd>Telescope live_grep<cr>') -- Find String
+vim.keymap.set('n', 'gs', '<cmd>Telescope grep_string<cr>') -- Grep String Under Cursor
+vim.keymap.set('n', 'fb', '<cmd>Telescope buffers<cr>') -- Find Open Files (in Buffer)
+vim.keymap.set('n', 'fc', '<cmd>Telescope oldfiles<cr>') -- Find Closed Files (in Buffer)
 vim.keymap.set('n', 'fh', '<cmd>Telescope help_tags<cr>')
 
 
