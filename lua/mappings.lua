03c383a54404dab5095070176d353026601ea4be
Chris Muga <chrismuga94@gmail.com>
2023-09-22 19:49:18 +0300

    sl = solarized-light sd = solarized-dark

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -19,4 +19,10 @@ vim.keymap.set('n', 'fc', '<cmd>Telescope oldfiles<cr>') -- Find Closed Files (i
 vim.keymap.set('n', 'fh', '<cmd>Telescope help_tags<cr>')
 vim.keymap.set('n', 'ft', '<cmd>Telescope colorscheme<cr>')
 
+-- Background Settings
+vim.keymap.set('n', 'bd', '<cmd>set bg=dark<cr>')
+vim.keymap.set('n', 'bl', '<cmd>set bg=light<cr>')
 
+-- Solarized Theme Settings
+vim.keymap.set('n', 'sd', '<cmd>set bg=dark<cr> <cmd>colorscheme solarized<cr>')
+vim.keymap.set('n', 'sl', '<cmd>set bg=light<cr> <cmd>colorscheme solarized<cr>')
