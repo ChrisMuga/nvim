821325377eaf4cdf77d1e63747e154427973373d
Chris Muga <ChrisMuga94@Gmail.com>
2024-04-06 20:30:56 +0300

    fix syntax error

--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -43,14 +43,14 @@ plugins = {
 	  }
 	  end,
 	  keys = {
-		  {'<leader>ff', '<cmd>Telescope find_files<cr>'}, -- Find file
-		  {'<leader>fs', '<cmd>Telescope live_grep<cr>'}, -- Find String
+			{'<leader>ff', '<cmd>Telescope find_files<cr>'}, -- Find file
+			{'<leader>fs', '<cmd>Telescope live_grep<cr>'}, -- Find String
 			{'<leader>gs', '<cmd>Telescope grep_string<cr>'},-- Grep String Under Cursor
 			{'<leader>fb', '<cmd>Telescope buffers<cr>'},-- Find Open Files (in Buffer)
 			{'<leader>fc', '<cmd>Telescope oldfiles<cr>'},-- Find Closed Files (in Buffer)
-			{'<leader>fh', '<cmd>Telescope help_tags<cr'},  
-			{'<leader>ft', '<cmd>Telescope colorscheme<cr'},  
-			{'<leader>fg', '<cmd>Telescope git_status<cr'},  
+			{'<leader>fh', '<cmd>Telescope help_tags<cr>'},  
+			{'<leader>ft', '<cmd>Telescope colorscheme<cr>'},  
+			{'<leader>fg', '<cmd>Telescope git_status<cr>'},  
 	  }
 	},
 	'nvim-pack/nvim-spectre',
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,14 +9,14 @@ vim.keymap.set('n','<C-t>', '<cmd>NERDTreeToggle<cr>')
 vim.keymap.set('n','<C-f>', '<cmd>NERDTreeFind<cr>')
 
 -- Fuzzy Finder Mappings
-vim.keymap.set('n', 'ff', '<cmd>Telescope find_files<cr>') -- Find Files
-vim.keymap.set('n', 'fs', '<cmd>Telescope live_grep<cr>') -- Find String
-vim.keymap.set('n', 'gs', '<cmd>Telescope grep_string<cr>') -- Grep String Under Cursor
-vim.keymap.set('n', 'fb', '<cmd>Telescope buffers<cr>') -- Find Open Files (in Buffer)
-vim.keymap.set('n', 'fc', '<cmd>Telescope oldfiles<cr>') -- Find Closed Files (in Buffer)
-vim.keymap.set('n', 'fh', '<cmd>Telescope help_tags<cr>')
-vim.keymap.set('n', 'ft', '<cmd>Telescope colorscheme<cr>')
-vim.keymap.set('n', 'fg', '<cmd>Telescope git_status<cr>')
+-- vim.keymap.set('n', 'ff', '<cmd>Telescope find_files<cr>') -- Find Files
+-- vim.keymap.set('n', 'fs', '<cmd>Telescope live_grep<cr>') -- Find String
+-- vim.keymap.set('n', 'gs', '<cmd>Telescope grep_string<cr>') -- Grep String Under Cursor
+-- vim.keymap.set('n', 'fb', '<cmd>Telescope buffers<cr>') -- Find Open Files (in Buffer)
+-- vim.keymap.set('n', 'fc', '<cmd>Telescope oldfiles<cr>') -- Find Closed Files (in Buffer)
+-- vim.keymap.set('n', 'fh', '<cmd>Telescope help_tags<cr>')
+-- vim.keymap.set('n', 'ft', '<cmd>Telescope colorscheme<cr>')
+-- vim.keymap.set('n', 'fg', '<cmd>Telescope git_status<cr>')
 
 -- Background Settings
 vim.keymap.set('n', 'bd', '<cmd>set bg=dark<cr>')
