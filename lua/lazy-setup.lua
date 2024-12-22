e325a675afa1df1ca55308dd72f63ad9790c3f40
Chris Muga <ChrisMuga94@Gmail.com>
2024-12-22 09:16:50 +0300

    More code refactor

--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -1,98 +1,102 @@
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = " " -- Same for `maplocalleader`
 local plugins = {
-		-- Treesitter
-		{
-			'nvim-treesitter/nvim-treesitter'
+	-- Treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+	},
+	-- Themes
+	-- https://github.com/EdenEast/nightfox.nvim
+	"EdenEast/nightfox.nvim",
+	"folke/tokyonight.nvim",
+	{ "rose-pine/neovim", name = "rose-pine" },
+	-- Catppuccin Theme
+	{ "catppuccin/nvim", name = "catppuccin" },
+	-- Solarized
+	"maxmx03/solarized.nvim",
+	"marko-cerovac/material.nvim",
+	-- Status Line
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup()
+		end,
+	},
+	-- Git Diff
+	"sindrets/diffview.nvim",
+	-- Git Blamer
+	{
+		"APZelos/blamer.nvim",
+		keys = {
+			{ "<leader>b", "<cmd>BlamerToggle<cr>" },
 		},
-		-- Themes
-		-- https://github.com/EdenEast/nightfox.nvim
-		'EdenEast/nightfox.nvim',
-		'folke/tokyonight.nvim',
-		{ "rose-pine/neovim", name = "rose-pine" },
-		-- Catppuccin Theme
-		{ "catppuccin/nvim", name="catppuccin" },
-		-- Solarized
-		'maxmx03/solarized.nvim',
-		'marko-cerovac/material.nvim',
-		-- Status Line
-		{
-			'nvim-lualine/lualine.nvim',
-			dependencies = { 'nvim-tree/nvim-web-devicons' },
-			config = function ()
-				require('lualine').setup()
-			end
-		},
-		-- Git Diff
-		'sindrets/diffview.nvim',
-		-- Git Blamer
-		{
-			'APZelos/blamer.nvim',
-			keys = {
-				{ '<leader>b', '<cmd>BlamerToggle<cr>' },
-			},
-		},
-		-- Neo Formatter
-		{
-			'sbdchd/neoformat',
-		},
-		'nvim-telescope/telescope-media-files.nvim',
-		-- Icons
-		'nvim-tree/nvim-web-devicons',
-		'lewis6991/gitsigns.nvim',
-		-- Fuzzy Finder (2)
-		'nvim-lua/plenary.nvim',
-		{'nvim-telescope/telescope.nvim',
-		config=function()
-			require "telescope".setup {
+	},
+	-- Neo Formatter
+	{
+		"sbdchd/neoformat",
+	},
+	"nvim-telescope/telescope-media-files.nvim",
+	-- Icons
+	"nvim-tree/nvim-web-devicons",
+	"lewis6991/gitsigns.nvim",
+	-- Fuzzy Finder (2)
+	"nvim-lua/plenary.nvim",
+	{
+		"nvim-telescope/telescope.nvim",
+		config = function()
+			require("telescope").setup({
 				defaults = {
 					preview = {
 						mime_hook = function(filepath, bufnr, opts)
 							local is_image = function(filepath)
-								local image_extensions = {'png','jpg', 'svg'}   -- Supported image formats
-								local split_path = vim.split(filepath:lower(), '.', {plain=true})
+								local image_extensions = { "png", "jpg", "svg" } -- Supported image formats
+								local split_path = vim.split(filepath:lower(), ".", { plain = true })
 								local extension = split_path[#split_path]
 								return vim.tbl_contains(image_extensions, extension)
 							end
 							if is_image(filepath) then
 								local term = vim.api.nvim_open_term(bufnr, {})
-								local function send_output(_, data, _ )
+								local function send_output(_, data, _)
 									for _, d in ipairs(data) do
-										vim.api.nvim_chan_send(term, d..'\r\n')
+										vim.api.nvim_chan_send(term, d .. "\r\n")
 									end
 								end
-								vim.fn.jobstart(
-								{
-									'catimg', filepath  -- Terminal image viewer command
-								}, 
-								{on_stdout=send_output, stdout_buffered=true, pty=true})
+								vim.fn.jobstart({
+									"catimg",
+									filepath, -- Terminal image viewer command
+								}, { on_stdout = send_output, stdout_buffered = true, pty = true })
 							else
-								require("telescope.previewers.utils").set_preview_message(bufnr, opts.winid, "Binary cannot be previewed")
+								require("telescope.previewers.utils").set_preview_message(
+									bufnr,
+									opts.winid,
+									"Binary cannot be previewed"
+								)
 							end
-						end
+						end,
 					},
 				},
 				pickers = {
 					colorscheme = {
-						enable_preview = true
-					}
-				}
-			}
+						enable_preview = true,
+					},
+				},
+			})
 		end,
 		keys = {
-			{'<leader><leader>', '<cmd>Telescope find_files<cr>'}, -- Find file
-			{'ff', '<cmd>Telescope find_files<cr>'}, -- Find file
-			{'fs', '<cmd>Telescope live_grep<cr>'}, -- Find String
-			{'gs', '<cmd>Telescope grep_string<cr>'},-- Grep String Under Cursor
-			{'fb', '<cmd>Telescope buffers<cr>'},-- Find Open Files (in Buffer)
-			{'fc', '<cmd>Telescope oldfiles<cr>'},-- Find Closed Files (in Buffer)
-			{'fh', '<cmd>Telescope help_tags<cr>'},
-			{'ft', '<cmd>Telescope colorscheme<cr>'},
-			{'fg', '<cmd>Telescope git_status<cr>'},
-			{'<leader>sa', 'y<ESC>:Telescope live_grep default_text=<c-r>0<CR>', mode = {"v"}}
-		}
+			{ "<leader><leader>", "<cmd>Telescope find_files<cr>" }, -- Find file
+			{ "ff", "<cmd>Telescope find_files<cr>" }, -- Find file
+			{ "fs", "<cmd>Telescope live_grep<cr>" }, -- Find String
+			{ "gs", "<cmd>Telescope grep_string<cr>" }, -- Grep String Under Cursor
+			{ "fb", "<cmd>Telescope buffers<cr>" }, -- Find Open Files (in Buffer)
+			{ "fc", "<cmd>Telescope oldfiles<cr>" }, -- Find Closed Files (in Buffer)
+			{ "fh", "<cmd>Telescope help_tags<cr>" },
+			{ "ft", "<cmd>Telescope colorscheme<cr>" },
+			{ "fg", "<cmd>Telescope git_status<cr>" },
+			{ "<leader>sa", "y<ESC>:Telescope live_grep default_text=<c-r>0<CR>", mode = { "v" } },
+		},
 	},
-	'nvim-pack/nvim-spectre',
+	"nvim-pack/nvim-spectre",
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
@@ -103,14 +107,14 @@ local plugins = {
 			"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
 		keys = {
-			{ '<C-t>', '<cmd>Neotree toggle<cr>' },
-			{ '<C-f>', '<cmd>Neotree reveal<cr>' }
+			{ "<C-t>", "<cmd>Neotree toggle<cr>" },
+			{ "<C-f>", "<cmd>Neotree reveal<cr>" },
 		},
-		lazy=false,
-		use_libuv_file_watcher=true,
-		config=function ()
+		lazy = false,
+		use_libuv_file_watcher = true,
+		config = function()
 			require("neo-tree").setup({
-				close_if_last_window=true,
+				close_if_last_window = true,
 				event_handlers = {
 					{
 						event = "neo_tree_window_after_open",
@@ -118,7 +122,7 @@ local plugins = {
 							if args.position == "left" or args.position == "right" then
 								vim.cmd("wincmd =")
 							end
-						end
+						end,
 					},
 					{
 						event = "neo_tree_window_after_close",
@@ -126,32 +130,32 @@ local plugins = {
 							if args.position == "left" or args.position == "right" then
 								vim.cmd("wincmd =")
 							end
-						end
-					}
+						end,
+					},
 				},
 				default_component_configs = {
 					git_status = {
 						symbols = {
 							-- Change type
-							added     = "✚", -- NOTE: you can set any of these to an empty string to not show them
-							deleted   = "✖",
-							modified  = "",
-							renamed   = "",
+							added = "✚", -- NOTE: you can set any of these to an empty string to not show them
+							deleted = "✖",
+							modified = "",
+							renamed = "",
 							-- Status type
 							untracked = "",
-							ignored   = "",
-							unstaged  = "",
-							staged    = "",
-							conflict  = "",
+							ignored = "",
+							unstaged = "",
+							staged = "",
+							conflict = "",
 						},
 						align = "right",
 					},
 				},
 			})
-		end
+		end,
 	},
 	-- Set Transparent Background
-	'xiyaowong/transparent.nvim',
+	"xiyaowong/transparent.nvim",
 	-- Surround
 	{
 		"kylechui/nvim-surround",
@@ -161,14 +165,14 @@ local plugins = {
 			require("nvim-surround").setup({
 				-- Configuration here, or leave empty to use defaults
 			})
-		end
+		end,
 	},
 	-- Comments
 	{
 		"terrortylor/nvim-comment",
 		config = function()
-			require('nvim_comment').setup()
-		end
+			require("nvim_comment").setup()
+		end,
 	},
 	-- Everforest theme
 	{
@@ -181,7 +185,7 @@ local plugins = {
 
 	-- Sort
 	{
-		'sQVe/sort.nvim',
+		"sQVe/sort.nvim",
 
 		-- Optional setup for overriding defaults.
 		config = function()
@@ -189,36 +193,27 @@ local plugins = {
 				-- Input configuration here.
 				-- Refer to the configuration section below for options.
 			})
-		end
+		end,
 	},
-	'mg979/vim-visual-multi',
+	"mg979/vim-visual-multi",
 	-- LSP
 	{
-		'williamboman/mason.nvim',
+		"williamboman/mason.nvim",
 	},
 	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
 		dependencies = {
 			--- Uncomment the two plugins below if you want to manage the language servers from neovim
-			{'williamboman/mason.nvim'},
-			'williamboman/mason-lspconfig.nvim',
+			{ "williamboman/mason.nvim" },
+			"williamboman/mason-lspconfig.nvim",
 			-- LSP Support
-			{'neovim/nvim-lspconfig'},
+			{ "neovim/nvim-lspconfig" },
 			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-buffer'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	},
-	{
-		"folke/trouble.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "L3MON4D3/LuaSnip" },
 		},
 	},
 	{
@@ -226,9 +221,9 @@ local plugins = {
 		opts = {
 			-- options
 		},
-		config= function ()
+		config = function()
 			require("fidget").setup({})
-		end
+		end,
 	},
 	-- which key
 	{
@@ -244,16 +239,16 @@ local plugins = {
 			-- refer to the configuration section below
 		},
 		keys = {
-			{ '<leader>w', '<cmd>Whichkey<cr>' },
+			{ "<leader>w", "<cmd>Whichkey<cr>" },
 		},
 	},
 	{
-		'akinsho/toggleterm.nvim',
+		"akinsho/toggleterm.nvim",
 		version = "*",
 		config = true,
 		keys = {
-			{'tt', '<cmd>:ToggleTerm direction=float name=Terminal<cr>'}
-		}
+			{ "tt", "<cmd>:ToggleTerm direction=float name=Terminal<cr>" },
+		},
 	},
 	{
 		"lukas-reineke/headlines.nvim",
@@ -262,32 +257,34 @@ local plugins = {
 	},
 	-- Scratch File
 	{
-	  "LintaoAmons/scratch.nvim",
-	  event = "VeryLazy",
+		"LintaoAmons/scratch.nvim",
+		event = "VeryLazy",
 	},
 	{
-		'brenoprata10/nvim-highlight-colors',
-		config=function ()
-			require('nvim-highlight-colors').setup({})
-		end
+		"brenoprata10/nvim-highlight-colors",
+		config = function()
+			require("nvim-highlight-colors").setup({})
+		end,
 	},
-		require("plugins.pomo"),
-		require("plugins.melange"),
-		require("plugins.todo-comments"),
-		require("plugins.lazy-git")
-	}
+	require("plugins.pomo"),
+	require("plugins.melange"),
+	require("plugins.todo-comments"),
+	require("plugins.lazy-git"),
+	require("plugins.trouble"),
+	require("plugins.stylua-nvim"),
+}
 
-			local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-			if not (vim.uv or vim.loop).fs_stat(lazypath) then
-				vim.fn.system({
-					"git",
-					"clone",
-					"--filter=blob:none",
-					"https://github.com/folke/lazy.nvim.git",
-					"--branch=stable", -- latest stable release
-					lazypath,
-				})
-			end
-			vim.opt.rtp:prepend(lazypath)
-			local opts = {}
-			require("lazy").setup(plugins, opts)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+local opts = {}
+require("lazy").setup(plugins, opts)
