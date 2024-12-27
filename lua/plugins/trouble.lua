d2833de6392257b3011617f3a492465314218162
Chris Muga <ChrisMuga94@Gmail.com>
2024-12-27 04:39:38 +0300

    Reformat

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,13 +1,20 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	lazy=false,
 	opts = {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 	},
-	lazy=false,
 	config = function()
 		require("trouble").setup()
 	end,
+	keys = {
+	 {
+		  "<leader>xx",
+		  "<cmd>Trouble diagnostics toggle<cr>",
+		  desc = "Diagnostics (Trouble)",
+		},
+	}
 }
