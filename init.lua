0d0b60ef6ff02d8efa72c0b5d10f569e31b84dc6
Chris Muga <ChrisMuga94@Gmail.com>
2026-04-04 21:35:28 +0300

    misc

--- init.lua
+++ init.lua
@@ -6,13 +6,12 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
-require('lazy-setup')
-require('settings')
+require("lazy-setup")
+require("settings")
 -- require('edenfox-feline')
-require('icons')
-require('gitsigns').setup()
-require('nightfox')
-require('mappings')
-require('nvim-treesitter-config')
-require('nvim-treesitter.install').compilers = { 'zig' }
-require('lsp')
+require("icons")
+require("gitsigns").setup()
+require("nightfox")
+require("mappings")
+require("nvim-treesitter.install").compilers = { "zig" }
+require("lsp")
--- lua/set-theme.lua
+++ lua/set-theme.lua
@@ -2,16 +2,19 @@ local tod = tostring(os.date("%p"))
 
 local is_am = string.lower(tod) == "am"
 
-local theme
+-- local theme = "material-deep-ocean"
+-- local theme = "rose-pine-main"
+-- local theme = "nordfox"
+local theme = "carbonfox"
 local mode
 
-if is_am then
-	mode = "light"
-	theme = "rose-pine-dawn"
-else
-	mode = "dark"
-	theme = "rose-pine-main"
-end
+-- if is_am then
+-- 	mode = "light"
+-- 	theme = "rose-pine-dawn"
+-- else
+-- 	mode = "dark"
+-- 	theme = "rose-pine-main"
+-- end
 
 vim.o.background = mode
 vim.cmd.colorscheme(theme)
